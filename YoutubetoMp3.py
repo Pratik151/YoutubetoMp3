9640d210cab10a452b923b2f5b5b860b91bbe4dc
Pratik151 <pratikpatel15133@gmail.com>
2016-08-21 11:26:48 +0530

    Changed API

--- YoutubetoMp3.py
+++ YoutubetoMp3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python2.7
+import requests
 import urllib2
 import os
 import pwd
@@ -9,21 +10,32 @@ import sys
 if len(sys.argv) < 2 or len(sys.argv) > 4:
    print "Usage: python YoutubetoMp3.py URL [path] [filename]"
    exit(0)
-urlLink = 'http://www.youtubeinmp3.com/fetch/?format=JSON&video=' + sys.argv[1]
+url = sys.argv[1]
+r = requests.post("http://www.listentoyoutube.com/cc/conversioncloud.php",data={"mediaurl": url, "client_urlmap": "none"})
+try:
+   statusurl = eval(r.text)['statusurl'].replace('\\/','/') + '&json'
+except:
+   print eval(r.text)['error']
+   time.sleep(1)
+while True:
+   try:
+      resp = eval(requests.get(statusurl).text)
+      if 'downloadurl' in resp:
+         downloadurl = resp['downloadurl'].replace('\\/','/')
+         title = resp['file']
+         break
+      time.sleep(1)
+   except Exception:
+      pass
 path = os.path.join('/home',pwd.getpwuid(os.getuid()).pw_name,'Music')
-response = urllib2.urlopen(urlLink)
-data = json.load(response)
-downloadLink = data['link']
-print downloadLink
-title = data['title']
-filename = title + '.mp3'
+filename = title
 if len(sys.argv) == 3:
    path = sys.argv[2]
 elif len(sys.argv) == 4:
    path = sys.argv[2]
    filename = sys.argv[3]
 print "Downloading %s" % (title)
-mp3file = urllib2.urlopen(downloadLink)
+mp3file = urllib2.urlopen(downloadurl)
 with open(os.path.join(path,filename), 'wb') as output:
    output.write(mp3file.read())
 print "File Successfully downloaded to : %s\nFilename : %s" % (path,filename)
