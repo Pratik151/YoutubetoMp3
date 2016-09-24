a3966215b4dbe7a0fae659f279f1e81f53bd6a7d
Pratik151 <pratikpatel15133@gmail.com>
2016-09-24 20:32:21 +0530

    Added feature to download by name of video

--- YoutubetoMp3/main.py
+++ YoutubetoMp3/main.py
@@ -5,12 +5,21 @@ import os
 import pwd
 import json
 import sys
+from bs4 import BeautifulSoup
 
 def main():
    if len(sys.argv) < 2 or len(sys.argv) > 4:
-      print "Usage: python YoutubetoMp3.py URL [path] [filename]"
+      print "Usage: python YoutubetoMp3.py URL/Name [path] [filename]"
       exit(0)
-   url = sys.argv[1]
+   if '.com' not in sys.argv[1]:
+      vidToSearch = sys.argv[1]
+      vidUrl = 'https://www.youtube.com/results?search_query=' + vidToSearch
+      response = urllib2.urlopen(vidUrl)
+      html = response.read()
+      soup = BeautifulSoup(html, "lxml")
+      url = 'https://www.youtube.com' + soup.find(attrs={'class':'yt-uix-tile-link'})['href']
+   else:
+      url = sys.argv[1]
    r = requests.post("http://www.listentoyoutube.com/cc/conversioncloud.php",data={"mediaurl": url, "client_urlmap": "none"})
    try:
       statusurl = eval(r.text)['statusurl'].replace('\\/','/') + '&json'
